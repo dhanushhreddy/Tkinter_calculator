af467d211fc1dffffadc37a76d26f14812bc9fe7
dhanushh reddy <dhanushhreddy748@gmail.com>
2026-04-28 13:40:50 +0530

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -6,7 +6,7 @@ def press(v):
     '''Called when a number or operator button is clicked
         Inserts the pressed value at end of Entry widget'''
     
-#clear fucntion
+#clear function
 def clear():
     entry.delete(0,tk.END)
     '''Clears the calculator screen 
@@ -43,11 +43,11 @@ root.configure(bg="#1e1e1e")
 
 root.resizable(False, False)
 
-#ENtry widegts (Display screen)
+#Entry widgets (Display screen)
 
 entry = tk.Entry(
     root,
-    font=("Times new Roman",20),
+    font=("Times New Roman",20),
     bg="#2d2d2d",
     fg="white",
     bd=0,
@@ -58,7 +58,7 @@ Right-aligned for better calculator look'''
 
 entry.grid(row=0, column=0, columnspan=4,padx=12, ipady=10)
 '''Place entry at top
-    columnspan=4 makes it stretch across 4 coloumns'''
+    columnspan=4 makes it stretch across 4 columns'''
 
 #Button labels
 buttons = [
@@ -68,7 +68,7 @@ buttons = [
     "0",".","=","+"
 ]
 
-'''Represent calculator buttons stored in list to reduce repetetive code'''
+'''Represent calculator buttons stored in list to reduce repetitive code'''
 
 r=1
 c=0
@@ -76,7 +76,7 @@ c=0
 
 for b in buttons: #iterates through each button label 
     cmd = calc if b == "=" else lambda x=b: press(x)
-    '''if button is "=", cal calc()
+    '''if button is "=", call calc()
     otherwise call press with the button value 
     lambda x=b prevents late binding issue '''
 
@@ -87,8 +87,8 @@ for b in buttons: #iterates through each button label
         font = ("Calibri",14),
         width = 5,
         height =2,
-        bg = "#ff9500" if b in "+_*/" else "#d44b4b",
-        #operator buttons are orange, number buttons are gray
+        bg = "#ff9500" if b in "+-*/" else "#d44b4b",
+        #operator buttons are orange, number buttons are red/gray
         fg = "black",
         bd = 0,
         ).grid(row=r, column = c, padx=6 , pady=6)
@@ -100,18 +100,18 @@ for b in buttons: #iterates through each button label
         c=0
         #Moves to next row after 4 buttons
 
-        #clear button
-        tk.Button(
-            root,
-            text="c",
-            command = clear,
-            font=("calibri",14),
-            bg="#f3bf3b",
-            fg="Black",
-            bd=0,
-            width=22,
-            height=2
-        ).grid(row=r, column=0, columnspan=4, pady=0)
+#clear button
+tk.Button(
+    root,
+    text="C",
+    command = clear,
+    font=("Calibri",14),
+    bg="#f3bf3b",
+    fg="Black",
+    bd=0,
+    width=22,
+    height=2
+).grid(row=r+1, column=0, columnspan=4, pady=10)
 '''Clears the calculator span across all columns'''
 
 #Event loop
